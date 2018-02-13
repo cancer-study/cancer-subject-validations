a0ce5f0c1a2dd0191a15366dffc00ea77b7348de
Itumeleng Mosweu <imosweuk@gmail.com>
2018-02-13 16:15:46 +0200

    update base risk cancer validation

--- cancer_subject_validations/form_validators/base_risk_assessment_cancer_form_validation.py
+++ cancer_subject_validations/form_validators/base_risk_assessment_cancer_form_validation.py
@@ -1,4 +1,4 @@
-from edc_base.modelform_validators import FormValidator
+from edc_form_validators import FormValidator
 from edc_constants.constants import YES
 
 
@@ -10,12 +10,16 @@ class BaseRiskAssessmentCancerFormValidation(FormValidator):
             YES,
             field='family_cancer',
             field_required='family_cancer_type',
+            required_msg=(
+                'If any relative has had any cancer, what type was it?')
         )
 
         self.required_if(
             YES,
             field='had_previous_cancer',
             field_required='previous_cancer',
+            required_msg=('If subject has had a previous cancer, '
+                          'what kind of cancer was it')
         )
 
         return self.cleaned_data
