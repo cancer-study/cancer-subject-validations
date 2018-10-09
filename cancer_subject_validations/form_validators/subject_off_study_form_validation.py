2eaaacb4ed3e71c2742b38df195a8fa310fb9d98
tumiemosweu <imosweuk@gmail.com>
2018-10-09 10:57:28 +0200

    clean slate

--- cancer_subject_validations/form_validators/subject_off_study_form_validation.py
+++
@@ -1,9 +0,0 @@
-from edc_form_validators import FormValidator
-from edc_constants.constants import YES
-
-
-class SubjectOffStudyFormValidation(FormValidator):
-
-    def clean(self):
-
-        pass
