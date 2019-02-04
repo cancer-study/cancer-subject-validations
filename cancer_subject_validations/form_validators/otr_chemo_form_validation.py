ebde6bee49cf4f9925d051c9d9f552538ec10ac4
Leslie <leslie.kep@gmail.com>
2019-02-04 17:02:04 +0200

    update oncology validator

--- cancer_subject_validations/form_validators/otr_chemo_form_validation.py
+++ cancer_subject_validations/form_validators/otr_chemo_form_validation.py
@@ -7,7 +7,7 @@ class OTRChemoFormValidation(FormValidator):
     def clean(self):
         self.required_if(
             YES,
-            field='chemo_delayed',
+            field='chemo_delays',
             field_required='why_delayed',)
 
         self.required_if(
