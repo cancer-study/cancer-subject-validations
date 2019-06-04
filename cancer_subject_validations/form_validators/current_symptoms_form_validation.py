091f2e6386650f60c68c0ed261ee62be8b5c6f60
Leslie <leslie.kep@gmail.com>
2019-06-04 15:40:06 +0200

    add applicable if validator

--- cancer_subject_validations/form_validators/current_symptoms_form_validation.py
+++ cancer_subject_validations/form_validators/current_symptoms_form_validation.py
@@ -6,7 +6,7 @@ class CurrentSymptomsFormValidation(FormValidator):
 
     def clean(self):
         required_fields = ['symptom_desc', 'patient_own_remedy',
-                           'ra_advice', 'severity']
+                           'ra_advice']
         for required in required_fields:
             if required in self.cleaned_data:
                 self.required_if(
@@ -14,3 +14,9 @@ class CurrentSymptomsFormValidation(FormValidator):
                     field='any_worry',
                     field_required=required,
                 )
+
+        self.applicable_if(
+            YES,
+            field='any_worry',
+            field_applicable='severity'
+        )
